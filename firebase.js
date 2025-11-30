2efc465da0dddfd0c02a931f8d0b12098fc54727
nansu31 <142662940+nansu31@users.noreply.github.com>
2025-11-30 21:46:18 +0700

    menambahkan firestore

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+    import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
--- script.js
+++ script.js
@@ -20,34 +20,3 @@ window.addEventListener('load', () => {
   showSection('fiqih');
   document.querySelector('[data-target="fiqih"]').classList.add('active');
 });
-
-fetch('assets/json/kemasyarakatan.json')
-  .then(res => res.json())
-  .then(data => {
-    const container = document.getElementById('konten-materi');
-    const title = document.createElement('h2');
-    title.textContent = data.judul;
-    container.appendChild(title);
-
-    data.materi.forEach(m => {
-      const section = document.createElement('section');
-      const h3 = document.createElement('h3');
-      h3.textContent = m.judul;
-      section.appendChild(h3);
-
-      const desc = document.createElement('p');
-      desc.textContent = m.deskripsi;
-      section.appendChild(desc);
-
-      const ul = document.createElement('ul');
-      m.isi.forEach(item => {
-        const li = document.createElement('li');
-        li.textContent = item;
-        ul.appendChild(li);
-      });
-      section.appendChild(ul);
-
-      container.appendChild(section);
-    });
-  });
-
